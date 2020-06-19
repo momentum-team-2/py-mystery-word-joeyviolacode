77b83366cf9edefc137b982345ddfa5788e85e59
joeyviolacode <joeyviolacode@gmail.com>
2020-06-19 15:42:19 -0400

    Frivolous intro sequence implemented.  Start to implementation of game flow/logic

--- mystery-word.py
+++ mystery-word.py
@@ -1,17 +1,75 @@
+import random
+
 file_name = "words.txt"
 
 words_file = open(file_name)
-MASTER_WORD_LIST = words_file.readlines()
+complete_words_list = words_file.readlines()
+MASTER_WORD_LIST = [word.replace("\n", "").upper() for word in complete_words_list]
+words_file.close()
+guessed_letters = []
 
-#explore mapping 1 and 0 to letters of words in subset:
+def start_game():
+    print()
+    #difficulty = get_difficulty()
+    #length = get_length()
+    difficulty = "E"
+    length = 10
+    word_list = get_words_of_length(length)
+    if difficulty == "E":
+        run_game_e(word_list)
+    else:
+        run_game_s(word_list)
+
+def run_game_e(word_list):
+    pass
+
+def run_game_s(word_list):
+    pass
 
 
+
+def get_difficulty():
+    difficulty = input("Welcome to Mystery Word!  Would you like to play the (E)asy or (S)inister version?\nOr would you just like to (Q)uit while you're ahead? ")
+    while difficulty is not "E" and difficulty is not "S" and difficulty is not "Q":
+        print("\nWell, that isn't an E or an S.  You know that this IS a spelling game, right?  Do you know your lettters?\nWait!  Do you know what letters ARE?")
+        print("Please try again, but just...be better this time.")
+        difficulty = input("\nWould you like to play the (E)asy or (S)inister version?  Or, you know, you can just give me a 'Q'.\nWhich is it? ")
+    if difficulty is "E":
+        print("\nOkay, but that seems a bit tame.\n")
+    elif difficulty is "S":
+        print("\nIt's your funeral.\n")
+    else:
+        print("\nThat's probably for the best, isn't it?\nIt'll certainly save us all a lot of trouble.\nStudy up and come back later.\nOr don't.  I don't really care.")
+        exit(1)
+    return difficulty
+
+def get_length():
+    length = int(input("Please let me know what length of word you'd like to try to solve.\nThis number should be between 3 and 24, inclusive: "))
+    while length < 3 or length > 24:
+        print("\n**SIGH**   Are you the same person who was having trouble with letters a few minutes back?")
+        print("No, no.  Don't answer.  I don't want to know.  Can you please just try again?")
+        length = input("But please do really, really try to give me a number between 3 and 24: ")
+    return length
+
+def get_words_of_length(length):
+    return [word for word in MASTER_WORD_LIST if len(word) == length]
+
+def get_word(word_list):
+    return word_list[random.randint(0, len(word_list) - 1)]
+
+def get_display_str_e():
+    str = ""
+    return str
+
+def get_display_str_s(word_list):
+    str = ""
+    return str
 
 
 
 
 def handle_family_selection(list_of_words, letter_to_check):
-    """Uses a collection of helper functions(below) to take in the current list of words and select a subset of that list based on
+    """Uses a collection of functions(below) to take in the current list of words and select a subset of that list based on
     criteria set in the select_family() function.  This is used to dodge the players guesses by (at this time) selecting the largest
     list of words that somehow contains the letter checked in some pattern"""
     words_keyed = map_key_to_word(list_of_words, letter_to_check)
@@ -19,23 +77,6 @@ def handle_family_selection(list_of_words, letter_to_check):
     sorted_words = sort_by_key(words_keyed, keys)
     family = select_family(sorted_words)
     return family
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 #The following are a set of functions called by handle_family_selection in order to select the next family of words to be used
 #by the game.....the commented pairs of variables and print provide testing options for seeing what happens at each stage 
@@ -130,10 +171,12 @@ def print_words_of_length(words_list, length):
             print(word)
 
 
+## Main function to start game logic
 if __name__ == "__main__":
-    test_words = ["teeth", "clear", "tooth", "beech", "beach", "teach", "peach", "place", "mango"]
-    print(test_words)
-    new_family = handle_family_selection(test_words, "a")
-    print(new_family)
-    newer_family = handle_family_selection(new_family, "p")
-    print(newer_family)
+    start_game()
+    # test_words = ["teeth", "clear", "tooth", "beech", "beach", "teach", "peach", "place", "mango"]
+    # print(test_words)
+    # new_family = handle_family_selection(test_words, "a")
+    # print(new_family)
+    # newer_family = handle_family_selection(new_family, "p")
+    # print(newer_family)
