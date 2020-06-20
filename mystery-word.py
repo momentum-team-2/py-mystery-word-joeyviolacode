93894a9accd7105b8197824aa353d39d16726c88
joeyviolacode <joeyviolacode@gmail.com>
2020-06-20 16:51:18 -0400

    Fully functional with Easy and Sinister in same program.  Needs poking at the edges to make sure all runs well.  Code needs cleaning, as there's lots of test stuff still there.

--- mystery-word.py
+++ mystery-word.py
@@ -4,18 +4,15 @@ file_name = "words.txt"
 
 words_file = open(file_name)
 complete_words_list = words_file.readlines()
-MASTER_WORD_LIST = [word.replace("\n", "").upper() for word in complete_words_list]
-TRIES_ALLOWED = 10
 words_file.close()
-guessed_letters = []
-wrong_tries = 3
+MASTER_WORD_LIST = [word.replace("\n", "").upper() for word in complete_words_list]
+TRIES_ALLOWED = 8
+
 
 def start_game():
     print()
-    #difficulty = get_difficulty()
-    #length = get_length()
-    difficulty = "E"
-    length = 10
+    difficulty = get_difficulty()
+    length = get_length()
     word_list = get_words_of_length(length)
     if difficulty == "E":
         run_game_e(get_word(word_list))
@@ -23,29 +20,86 @@ def start_game():
         run_game_s(word_list)
 
 def run_game_e(word):
-    print(get_display_str("PASTE"))
-    print("Easy is running.")
+    guessed_letters = []
+    wrong_tries = 0
+    while True:
+        display_word = get_display_str(word, guessed_letters)
+        if no_blanks(display_word):
+            print("You win!!!  I didn't think you could do it.  Surprises all around.  Your word was: " + word + "\n\n")
+            play_again()
+        guess = get_guess(display_word, wrong_tries, guessed_letters)
+        guessed_letters.append(guess)
+        if guess in word:
+            print("\nGood guess!  That's in the word!")
+        else:
+            print("\nSorry.  That's not a letter in the word you're trying to guess.")
+            wrong_tries += 1
+        if wrong_tries == TRIES_ALLOWED:
+            print("You've taken too many guesses.  Unfortunately, you have not won the day.\n")
+            print("The word that you were trying to figure out was: " + word)
+            print("It's kind of obvious now that you see it, isn't it?")
+            play_again()
+
 
 def run_game_s(word_list):
-    print(get_display_str(word_list))
-    print("Sinister is running.")
+    print("The word list currently contains " + str(len(word_list)) + " words.")
+    guessed_letters = []
+    wrong_tries = 0
+    while True:
+        display_word = get_display_str(word_list, guessed_letters)
+        if no_blanks(display_word):
+            print("You win!!!  I didn't think you could do it.  Surprises all around.  Your word was: " 
+                    + word_list[random.randint(0, len(word_list) - 1)] + "\n\n")
+            play_again()
+        guess = get_guess(display_word, wrong_tries, guessed_letters)
+        guessed_letters.append(guess)
+        word_list = handle_family_selection(word_list, guess)
+        print("The word list currently contains " + str(len(word_list)) + " words.")
+        if guess in word_list[0]:
+            print("\nGood guess!  That's in the word!")
+        else:
+            print("\nSorry.  That's not a letter in the word you're trying to guess.")
+            wrong_tries += 1
+        if wrong_tries == TRIES_ALLOWED:
+            print("You've taken too many guesses.  Unfortunately, you have not won the day.\n")
+            print("The word that you were trying to figure out was: " + word_list[random.randint(0, len(word_list) - 1)])
+            print("It's kind of obvious now that you see it, isn't it?")
+            play_again()
+
+def play_again():
+    print()
+    print("That was...exciting?  Please don't feel like you have to, but do you want to play again?")
+    print("Don't feel like you have to just to make me feel better.\nI'm completely happy to free up some memory and just catch some sleep.")
+    print("Anyway, let's make this easy on all of us.  Press Y for (Y)es if you want to play again.")
+    again = input("If you don't want to play again, press anything at all other than Y.   ").upper()
+    if again == "Y":
+        print("Starting")
+        start_game()
+    else:
+        print("exiting")
+        exit()
+
+def no_blanks(display_word):
+    return "_" not in display_word
 
 
-def get_guess(word):
+def get_guess(display_word, wrong_tries, guessed_letters):
     """This function takes in a word (or list of words in Sinister), displays the information known so far about the guesses that have
         been made and then prompts the user for a letter.  I makes sure that it receives a letter, and if so, returns a capitalized
         version of that letter to the caller"""
     print()
     print(f"You're allowed only {TRIES_ALLOWED - wrong_tries} more wrong guesses.  Be very careful.")
     print("Here's what you know so far about the word you're trying to guess:\n")
-    print(get_display_str(word) + "\n\n")
+    print(display_word + "\n\n")
     guess = ""
     if len(guessed_letters) == 0:
         guess = input("Make your first guess.  In case it helps, this should be a letter, one of the ones in the alphabet: ")
     else: 
+        print("You have already used: " + ", ".join(guessed_letters))
         guess = input("Time to make another guess: ")
-    while not ("A" <= guess <= "z"):
-        print("Um...I'm afraid that isn't a letter.  You'll find letters kind of in the big middle part of your keyboard.\n")
+    while not ("A" <= guess <= "z") or guess.upper() in guessed_letters:
+        print("\nUm...I'm afraid that isn't a letter.  Or you've already picked that one.  I don't know.")
+        print("You'll find letters kind of in the big middle part of your keyboard.\n")
         guess = input("Try again: ")
     return guess.upper()
 
@@ -74,24 +128,27 @@ def get_length():
     return length
 
 def get_words_of_length(length):
+    """Returns a list of words of a given length.  This will be the starting master family for Sinister and the pool from which 
+        a word will be chosen for easy difficulty"""
     return [word for word in MASTER_WORD_LIST if len(word) == length]
 
 def get_word(word_list):
+    """Pulls a random word out of the word list.  This is used by the easy version to select a word at random for the player to 
+        guess.  It is used by the Sinister version to select a display word at the end of the game when the player has failed to narrow
+        the families of words down to a single word"""
     return word_list[random.randint(0, len(word_list) - 1)]
 
-def get_display_str(word):
+def get_display_str(word_or_list, guessed_letters):
     """Returns a string representation of guessed letters and blanks for unguessed letters to show the player for the chosen word"""
-    if isinstance(word, list):
-        word = word[0]
+    if isinstance(word_or_list, list):
+        word_or_list = word_or_list[0]
     str = ""
-    for letter in word:
+    for letter in word_or_list:
         if letter in guessed_letters:
             str += letter + " "
         else:
             str += "_ "
     return str
-
-
 
 
 def handle_family_selection(list_of_words, letter_to_check):
@@ -199,7 +256,10 @@ def print_words_of_length(words_list, length):
 
 ## Main function to start game logic
 if __name__ == "__main__":
-    get_guess("PASTE")
+    start_game()
+    #run_game_s(["TEETH", "CLEAR", "TOOTH", "BEECH", "BEACH", "TEACH", "PEACH", "PLACE", "MANGO"])
+    #get_guess(get_display_str("PASTE", ["P", "S"]), 3, ["P", "S"])
+    #print(no_blanks("P E A R T"))
     #start_game()
     # test_words = ["teeth", "clear", "tooth", "beech", "beach", "teach", "peach", "place", "mango"]
     # print(test_words)
